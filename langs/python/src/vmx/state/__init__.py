3d52ab62e245e58708a4ff4051ac727b0e540c4e
Kaveh Razavi <kaveh.razavi@gmail.com>
2026-07-12 13:35:10 -0400

    feat(python): add AsyncResourceVM (#139)

--- langs/python/src/vmx/state/__init__.py
+++ langs/python/src/vmx/state/__init__.py
@@ -1,5 +1,29 @@
 """State helper ViewModels."""
 
+from vmx.state.async_resource_vm import (
+    AsyncResourceError,
+    AsyncResourceErrorWithValue,
+    AsyncResourceIdle,
+    AsyncResourceLoading,
+    AsyncResourceLoadingWithValue,
+    AsyncResourceReady,
+    AsyncResourceRetention,
+    AsyncResourceState,
+    AsyncResourceStatus,
+    AsyncResourceVM,
+)
 from vmx.state.discriminator_vm import DiscriminatorVM
 
-__all__ = ["DiscriminatorVM"]
+__all__ = [
+    "AsyncResourceError",
+    "AsyncResourceErrorWithValue",
+    "AsyncResourceIdle",
+    "AsyncResourceLoading",
+    "AsyncResourceLoadingWithValue",
+    "AsyncResourceReady",
+    "AsyncResourceRetention",
+    "AsyncResourceState",
+    "AsyncResourceStatus",
+    "AsyncResourceVM",
+    "DiscriminatorVM",
+]
